bdef9f060b1d3b7f975c2d69f841eab4feb8d0cd
Tien Dao <daonamtien@gmail.com>
2017-02-25 21:46:28 +0700

    Cm

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const MyComponents = {
+  DatePicker: function DatePicker(props) {
+    return <div>Imagine a {props.color} datepicker here.</div>;
+  },
+  Date: function Date(props){
+    return <div>Imagine a {props.color} date here. </div>;
+  }
+}
+function NumberDescriber(props) {
+  let description;
+  if (props.number % 2 === 0) {
+    description = <strong>even</strong>;
+  } else {
+    description = <i>odd</i>;
+  }
+  return <div>{props.number} is an {description} number</div>;
+}
+// Calls the children callback numTimes to produce a repeated component
+function Repeat(props) {
+  let items = [];
+  for (let i = 0; i < props.numTimes; i++) {
+    items.push(props.children(i));
+  }
+  return <div>{items}</div>;
+}
 
+function ListOfTenThings() {
+  return (
+    <Repeat numTimes={10}>
+      {(index) => <div key={index}>This is item <strong>{index}</strong> in the list</div>}
+    </Repeat>
+  );
+}
 class App extends Component {
   render() {
     return (
@@ -12,6 +44,10 @@ class App extends Component {
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <MyComponents.DatePicker color="blue" />
+          <MyComponents.Date color="red" />
+          <NumberDescriber number="5" />
+          <ListOfTenThings />
         </p>
       </div>
     );
